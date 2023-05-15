b9a2420f65b30c905be4cc4594202918d210edf6
PrzeChoj <premysl.choj@gmail.com>
2023-05-15 21:34:58 +0200

    alg `z_papiera_GLASSO` implementacja i dziala

--- 01_algorytm_GLASSO.R
+++ 01_algorytm_GLASSO.R
@@ -12,7 +12,7 @@ T_function <- function(x, lambda){
   return(sign(x)*max(abs(x)-lambda,0))
 }
 
-my_GLASSO <- function(my_cov, lambda){
+z_zajec_GLASSO <- function(my_cov, lambda, maxiter = 100){
   stopifnot(lambda > 0)
   stopifnot(is.positive.semi.definite.matrix(my_cov))
   
@@ -23,16 +23,20 @@ my_GLASSO <- function(my_cov, lambda){
   B <- matrix(numeric(p*p), ncol = p) # macierz zer
   
   continue_outer <- TRUE
+  counter_outer <- 0
   v <- p
   while (continue_outer){ # this will stop if no changes were made to sigma_matrix
+    counter_outer <- counter_outer + 1
     continue_outer <- FALSE # this will be changed to TRUE if anything will change in B or sigma
     v <- ifelse(v == p, 1, v + 1)
     
     continue_inner <- TRUE
+    counter_inner <- 0
     u <- p
     counter_changes_B <- -1 # will be changed in the 1st run of the while
     
     while (continue_inner){
+      counter_inner <- counter_inner + 1
       if (u == p){
         if(counter_changes_B == 0){
           continue_inner <- FALSE # zbieglismy juz
@@ -52,16 +56,24 @@ my_GLASSO <- function(my_cov, lambda){
       old_B_u_v <- B[u,v]
       my_sum <- sum(sigma_matrix[u,-v] * B[-v,v])
       B[u,v] <- T_function(my_cov[u,v] - my_sum, lambda)/sigma_matrix[v,v]
+      B[v,u] <- B[u,v]
       
+      ic(abs(old_B_u_v - B[u,v]))
       if(abs(old_B_u_v - B[u,v]) > 0.001){
         counter_changes_B <- counter_changes_B + 1
         continue_outer <- TRUE
       }
+      
+      if(counter_inner > maxiter){
+        continue_inner <- FALSE
+      }
     }
     
     continue_inner <- TRUE
+    counter_inner <- 0
     counter_changes_sigma <- 1
     while (continue_inner){
+      counter_inner <- counter_inner + 1
       if (u == p){
         if(counter_changes_sigma == 0){
           continue_inner <- FALSE # zbieglismy juz
@@ -80,11 +92,87 @@ my_GLASSO <- function(my_cov, lambda){
       
       old_sigma_u_v <- sigma_matrix[u,v]
       sigma_matrix[u,v] <- sum(sigma_matrix[u,-v] * B[-v,v])
+      sigma_matrix[v,u] <- sigma_matrix[u,v]
       
       if(abs(old_sigma_u_v - sigma_matrix[u,v]) > 0.001){
         counter_changes_sigma <- counter_changes_sigma + 1
         continue_outer <- TRUE
       }
+      
+      if(counter_inner > maxiter){
+        continue_inner <- FALSE
+      }
+    }
+    
+    if(counter_outer > maxiter){
+      continue_outer <- FALSE
+    }
+  }
+  
+  # Teraz budujemy macierz K.
+  my_K <- matrix(numeric(p*p), nrow=p) #inicjalizujemy
+  
+  for (v in 1:p) {
+    my_K[v,v] <- 1/(sigma_matrix[v,v] - sum(sigma_matrix[v,-v] * B[-v,v]))
+  }
+  
+  for (v in 1:p) {
+    for (u in 1:p) {
+      if (u == v){
+        next
+      }
+      else{
+        my_K[u,v] <- -B[u,v] * my_K[v,v]
+      }
+    }
+  }
+  
+  my_K
+}
+
+z_papiera_GLASSO <- function(my_cov, lambda, maxiter = 100){
+  stopifnot(lambda > 0)
+  stopifnot(is.positive.semi.definite.matrix(my_cov))
+  
+  p <- ncol(my_cov) # wymiar macierzy
+  
+  sigma_matrix <- my_cov + diag(lambda, nrow = p) # macierz precyzji
+  # Diagonal are already ok
+  B <- matrix(numeric(p*p), ncol = p) # macierz zer
+  
+  
+  
+  
+  v <- p
+  old_sigma_matrix <- sigma_matrix
+  stop_crit <- FALSE
+  num_iter <- 0
+  while(!stop_crit && num_iter < maxiter){
+    v <- ifelse(v == p, 1, v + 1)
+    
+    tryCatch(x <- expm::sqrtm(sigma_matrix[-v,-v]),
+             error = function(e) {browser()}) # sometimes this surprisingly happened
+    y <- Matrix::solve(x, my_cov[-v,v])
+    
+    coefs <- unclass(coef(glmnet(x, y, lambda = lambda, intercept = FALSE)))
+    my_beta <- numeric(p-1)
+    for (i in 1:(p-1)) {
+      if(i %in% attr(coefs, "i")){
+        my_beta[i] <- attr(coefs, "x")[attr(coefs, "i") == i]
+      }
+    }
+    
+    B[v, -v] <- my_beta
+    
+    sigma_matrix[-v,v] <- sigma_matrix[-v,-v] %*% my_beta
+    sigma_matrix[v,-v] <- t(sigma_matrix[-v,v])
+    
+    if(v == p){
+      num_iter <- num_iter + 1
+      if(sum(abs(old_sigma_matrix - sigma_matrix)) < 0.001){
+        stop_crit <- TRUE
+      }
+      old_sigma_matrix <- sigma_matrix
     }
   }
   
--- 03_analiza_wynikow.R
+++ 03_analiza_wynikow.R
@@ -1,3 +1,13 @@
 source("./01_algorytm_GLASSO.R")
-source("./02_wczytanie_danych.R")
+#source("./02_wczytanie_danych.R")
 
+set.seed(234567)
+p <- 50
+lambda <- 0.1
+my_cov <- matrix(rnorm(p*p), ncol=p)
+my_cov <- my_cov %*% t(my_cov)
+my_cov
+
+K <- z_papiera_GLASSO(my_cov,lambda)
+
+K
